e40be75479d9f2a28557d2c2e6f83a90f932e817
Srinidhi011 <74104028+Srinidhi011@users.noreply.github.com>
2020-11-07 21:42:03 +0530

    Update srinidhi-app.py

--- srinidhi-app.py
+++ srinidhi-app.py
@@ -8,7 +8,7 @@ def create_api():
 
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
-
+ 
   api = tweepy.API(auth)
  # api.verify_credentials()
   print('API Created')
